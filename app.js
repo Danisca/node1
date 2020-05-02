c98135b026546b084e7e8e8987bd8e1ca2aa8ea4
danis94 <dcanadablandon@gmail.com>
2020-05-02 16:41:05 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/views/learn_more.html",function(req,res){
+    res.sendFile(__dirname + "/views/learn_more.html");
+});
+
+app.listen(3000,function(){
+    console.log("server is running at 3000 port");
+    
+});
